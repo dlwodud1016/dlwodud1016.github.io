962e9a8c8026a28ce40c4a5c27400d23030c7d36
Jaeyoung Lee <jylee@adenasoft.com>
2025-10-23 12:21:59 +0900

    feat: fix asset prefix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: "export",
+    reactStrictMode: true,
+    output: "export",
+    assetPrefix: isProd ? "https://dlwodud1016.github.io" : undefined,
 };
 
 export default nextConfig;
